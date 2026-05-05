41b7d204240b12507a3894d10a38220ab63355f8
Raegan Scheet <CamScheet@gmail.com>
2026-05-04 23:00:48 -0500

    typescrip

--- Assets/Scripts/MapHighlights/TypeScript_Words.cs
+++ Assets/Scripts/MapHighlights/TypeScript_Words.cs
@@ -17,7 +17,7 @@ public class TypeScript_Words : MonoBehaviour
 
     private IEnumerator NextLine()
     {
-        yield return new WaitForSeconds(0.05f);
         writer.ReplaceText("Testing: Day 846, 7 AM \n> The pain response tests revealed surprising results... The experiment seemed to show signs of emotion. Specifically, rage. I'm lucky she's trapped behind that glass. Roe Jogan would put an end to her almost immediately, should she escape. We wouldn't want her going down the drains and getting to the mainframe. \nWelp, I'm off to lunch! I sure hope nothing happens while I'm gone! ");
+        yield return new WaitForSeconds(0f);
     }
 }
